a46d0bf3f8ea9678e99c3eb45535ce387606cc42
Andrea Esposito <esposito_andrea99@hotmail.com>
2022-03-18 12:12:04 +0100

    Add parallelism to create-train-test

--- create-train-test.py
+++ create-train-test.py
@@ -2,12 +2,14 @@
 import argparse
 import shutil
 from collections import namedtuple
+from functools import partial
 from pathlib import Path
 import random
 from typing import List
 
 import pandas as pd
 import yaml
+from p_tqdm import p_umap
 
 
 def setup_args() -> argparse.Namespace:
@@ -36,13 +38,13 @@ SAFE_COLS = {"ClusterLatitude", "ClusterLongitude", "dateTimeGroup"}
 Edit = namedtuple("Edit", ["row", "column", "old_value", "new_value"])
 
 
-def perturb(split: Path) -> List[Edit]:
+def perturb(split: Path, fraction: float = 0.1, seed: int = 42) -> List[Edit]:
     history = []
-    shutil.copy(split / 'test.csv', split / 'test.csv.bak')
+    shutil.copy(split / 'test.csv', split / 'test.csv.orig')
     df = pd.read_csv(split / 'test.csv')
     unsafe_cols = list(set(df.columns) - SAFE_COLS)
     min_max = df.aggregate(['min', 'max'])
-    perturbed_data = df.sample(frac=args.fraction, random_state=args.seed)
+    perturbed_data = df.sample(frac=fraction, random_state=seed)
     for i, data in perturbed_data.iterrows():
         chosen_cols = random.choices(unsafe_cols, k=random.randint(1, len(unsafe_cols)))
         for col in chosen_cols:
@@ -58,7 +60,7 @@ def perturb(split: Path) -> List[Edit]:
         perturbed_data.loc[i] = data
     df.update(perturbed_data)
     df.to_csv(split / 'test.csv', index=False)
-    return history
+    return list(filter(lambda x: x.old_value != x.new_value, history))
 
 
 def generate_train_set(folder: Path, files: List[Path]):
@@ -73,24 +75,29 @@ def generate_train_set(folder: Path, files: List[Path]):
                     dest.write(line)
 
 
+def generate_split(output: Path, files: List[Path], i: int, fraction: float = 0.1, seed: int = 42):
+    folder = output / f"split-{i}"
+    folder.mkdir(exist_ok=True, parents=True)
+    generate_train_set(folder, files[0:i])
+    shutil.copy(files[i], folder / 'test.csv')
+    with open(folder / 'contained-windows.yaml', "w") as f:
+        yaml.safe_dump({
+            'training-windows': [str(x) for x in files[0:i]],
+            'testing-window': str(files[i])
+        }, f)
+    history = perturb(folder, fraction=fraction, seed=seed)
+    pd.DataFrame(data=history).to_csv(folder / 'perturbations.csv', index=False)
+
+
 def main():
     args = setup_args()
     directory = Path(args.directory)
     output = Path(args.output)
+    shutil.rmtree(output, ignore_errors=True)
     output.mkdir(exist_ok=True, parents=True)
     files = sorted(directory.glob('window-*.csv'), key=lambda x: int(str(x.name)[7:-4]))
-    for i in range(1, len(files)):
-        folder = output / f"split-{i}"
-        folder.mkdir(exist_ok=True, parents=True)
-        generate_train_set(folder, files[0:i])
-        shutil.copy(files[i], folder / 'test.csv')
-        with open(folder / 'contained-windows.log', "w") as f:
-            yaml.safe_dump({
-                'training-windows': [str(x) for x in files[0:i]],
-                'testing-window': str(files[i])
-            }, f)
-        history = perturb(folder)
-        pd.DataFrame(data=history).to_csv(folder / 'perturbations.csv', index=False)
+    func = partial(generate_split, output, files, fraction=args.fraction, seed=args.seed)
+    p_umap(func, range(1, len(files)))
 
 
 if __name__ == '__main__':
